7382a77b24fac619c2a4b3815b5d8534b2d04971
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-11-08 17:01:13 -0300

    Implementando contextos modularizados en la aplicación de rutas

--- clase-9/src/App.jsx
+++ clase-9/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-import { AppProvider } from './context/AppContext'
+import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
 import Navbar from './components/Navbar'
 import Inicio from './pages/Inicio'
 import Servicios from './pages/Servicios'
@@ -8,6 +9,8 @@ import DetalleProducto from './pages/DetalleProducto'
 import IniciarSesion from './pages/IniciarSesion'
 import RutaProtegida from './pages/RutaProtegida'
 import Pagar from './pages/Pagar'
+import Dashboard from './pages/Dashboard'
+import FormProductos from './components/FormProductos'
 import Nosotros from './pages/Nosotros'
 import Contacto from './pages/Contacto'
 import Footer from './components/Footer'
@@ -16,27 +19,44 @@ import './App.css'
 function App() {
 
   return (
-    <AppProvider>
-      <>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Inicio />} />
-          <Route path='/servicios' element={<Servicios />} />
-          <Route path='/productos' element={<Productos />} />
-          <Route path='/productos/:id' element={<DetalleProducto />} />
-          <Route path='/productos/:categoria/:id' element={<DetalleProducto />} />
-          <Route path='/iniciar-sesion' element={<IniciarSesion />} />
-          <Route path='/pagar' element={
-            <RutaProtegida>
-              <Pagar />
-            </RutaProtegida>
-          } />
-          <Route path='/nosotros' element={<Nosotros />} />
-          <Route path='/contacto' element={<Contacto />} />
-        </Routes>
-        <Footer />
-      </>
-    </AppProvider>
+    <div>
+      <AuthProvider>
+        <CartProvider>
+          <>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Inicio />} />
+              <Route path='/servicios' element={<Servicios />} />
+              <Route path='/productos' element={<Productos />} />
+              <Route path='/productos/:id' element={<DetalleProducto />} />
+              <Route path='/productos/:categoria/:id' element={<DetalleProducto />} />
+              {/* Ruta protegida - Solo Usuarios */}
+              <Route path='/iniciar-sesion' element={<IniciarSesion />} />
+              <Route path='/pagar' element={
+                <RutaProtegida>
+                  <Pagar />
+                </RutaProtegida>
+              } />
+              {/* Ruta protegia - Solo Admin */}
+              <Route path='/dashboard' element={
+                <RutaProtegida soloAdmin={true}>
+                  <Dashboard />
+                </RutaProtegida>
+              } />
+              <Route path='/agregar-productos' element={
+                <RutaProtegida soloAdmin={true}>
+                  <FormProductos />
+                </RutaProtegida>
+              } />
+              {/* -------------------------- */}
+              <Route path='/nosotros' element={<Nosotros />} />
+              <Route path='/contacto' element={<Contacto />} />
+            </Routes>
+            <Footer />
+          </>
+        </CartProvider>
+      </AuthProvider>
+    </div>
   )
 }
 
